6a6671906c1af9967ba43252b9ba22ff0c9be1f5
ELKondradoSefu <radu.condrat@student.upt.ro>
2023-01-19 21:33:50 +0200

    cleaner code plus some basic stuff on reviews

--- src/main/java/com/user/planeapp/UserLogin.java
+++ src/main/java/com/user/planeapp/UserLogin.java
@@ -14,12 +14,12 @@ public class UserLogin extends Application {
 
     public static final String CURRENCY="$";
     @Override
-            public void start(Stage primaryStage) throws Exception{
-             Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Interface.fxml")));
-             primaryStage.initStyle(StageStyle.UNDECORATED);
-             primaryStage.setTitle("Plane Market");
-             primaryStage.setScene(new Scene(root,1350,750));
-             primaryStage.show();
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Interface.fxml")));
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.setTitle("Plane Market");
+        primaryStage.setScene(new Scene(root,1350,750));
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
